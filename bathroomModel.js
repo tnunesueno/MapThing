2ae402bfae502155e200fe26f3f94b1f874ca559
tnune <tnunesueno25@scienceleadership.org>
2025-03-04 17:21:08 -0500

    committing to kill this firebase nonsense

--- bathroomModel.js
+++ bathroomModel.js
@@ -1,3 +1,62 @@
+// Import the functions you need from the SDKs you need
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {auth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQCJP0lTLWp4gRzh6bv_YZv9EmndPSkCc",
+  authDomain: "where2wizz.firebaseapp.com",
+  projectId: "where2wizz",
+  storageBucket: "where2wizz.firebasestorage.app",
+  messagingSenderId: "491577614536",
+  appId: "1:491577614536:web:52965a10ae2d325a3e45fd",
+  measurementId: "G-HLEL0645BV"
+};
+
+// FIREBASE STUFF DOES NOT WORK 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+document.addEventListener('DOMContentLoaded', function() {
+  const loadEl = document.querySelector('#load');
+  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+  // // The Firebase SDK is initialized and available here!
+  // firebase.auth().onAuthStateChanged(user => { });
+  // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+  // firebase.firestore().doc('/foo/bar').get().then(() => { });
+  // firebase.functions().httpsCallable('yourFunction')().then(() => { });
+  // firebase.messaging().requestPermission().then(() => { });
+  // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+  // firebase.analytics(); // call to activate
+  // firebase.analytics().logEvent('tutorial_completed');
+  // firebase.performance(); // call to activate
+  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+  
+
+  try {
+  let app = firebase.app();
+  let features = [
+    'auth', 
+    'database', 
+    'firestore',
+    'storage', 
+  ].filter(feature => typeof app[feature] === 'function');
+  loadEl.textContent = `Firebase SDK loaded with ${features.join(', ')}`;
+} catch (e) {
+  console.error(e);
+  loadEl.textContent = 'Error loading the Firebase SDK, check the console.';
+}
+});
+
 class Bathroom {
     constructor(name, streetAddress, bLatitude, bLongitude, cleanliness, handicapAccesible, babyChangingStation, genderNeutral, notes) {
         this.name = name;  
@@ -114,34 +173,6 @@ array.push(bathroom10);
 array.push(bathroom11);
 array.push(bathroom12);
 array.push(bathroom13);
-
-// Import the functions you need from the SDKs you need
-
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBQCJP0lTLWp4gRzh6bv_YZv9EmndPSkCc",
-  authDomain: "where2wizz.firebaseapp.com",
-  projectId: "where2wizz",
-  storageBucket: "where2wizz.firebasestorage.app",
-  messagingSenderId: "491577614536",
-  appId: "1:491577614536:web:52965a10ae2d325a3e45fd",
-  measurementId: "G-HLEL0645BV"
-};
-
-
-// FIREBASE STUFF DOES NOT WORK 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 // Initialize and add the map
 let map;
