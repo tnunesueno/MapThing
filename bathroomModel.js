21a1ed92d7b80b9e0c901cca10314fcae91f487f
tnune <tnunesueno25@scienceleadership.org>
2025-04-12 10:05:46 -0400

    one fetch line

--- bathroomModel.js
+++ bathroomModel.js
@@ -1,3 +1,5 @@
+import {writeOneBr} from './firebase.js'
+import { fetchBathrooms } from './firebase.js';
 class Bathroom {
     constructor(name, streetAddress, bLatitude, bLongitude, cleanliness, handicapAccesible, babyChangingStation, genderNeutral, notes) {
         this.name = name;  
@@ -84,37 +86,6 @@ class Bathroom {
     }
 
 }
-
-// the addresses are the only true values here. remember to find a way to populate the rest with real data and put it in info windows??
-const bathroom1 = new Bathroom(null, "207 S. Sydenham St",null, null, 10,true,false,true, null)
-const bathroom2 = new Bathroom(null, "2000 Sansom Street",null, null, 10, false, true, false, null);
-const bathroom3 = new Bathroom(null," 1937 Callowhill St", null, null, 10, true, false, true, null);
-const bathroom4 = new Bathroom("Mt. Airy Coffee", "7101 Emlen St", null, null, 10, false, true, false, null);
-const bathroom5 = new Bathroom(null, "923 Race St", null, null, 10, true, false, true, null);
-const bathroom6 = new Bathroom("Liberty Place Food Court", "1625 Chestnut St", null, null, 8, true, true, false, "Bathroom on hallway between Fuwa and Bain’s Deli");
-const bathroom7 = new Bathroom("Just Salad", "1729 Chestnut St", null, null, 8, true, false, true, "Code: 9532. No inside lock. Single stall.");
-const bathroom8 = new Bathroom("DIG", "1616 Chestnut St", null, null, 8, true, true, true, "Code: 2929. Single stall.");
-const bathroom9 = new Bathroom("Di Bruno Bros", "1730 Chestnut Street", null, null, 8, true, false, true, "Bathroom behind elevator access door.");
-const bathroom10 = new Bathroom("Cheesecake Factory", "1430 Walnut St", null, null, 10, true, true, false, "Bathroom is through dining room, which is upstairs. Elevator available.");
-const bathroom11 = new Bathroom("CAVA", "1713 Chestnut St", null, null, 8, true, true, true, "Code: 09876.");
-const bathroom12 = new Bathroom("Capital One Café", "135 S 17th St", null, null, 9, true, true, false, "Bathroom upstairs.");
-const bathroom13 = new Bathroom("Ten Asian Food Hall", "1715 Chestnut St", null, null, 7, true, false, false, "notes");
-
-var array = []; 
-array.push(bathroom1);
-array.push(bathroom2);
-array.push(bathroom3);
-array.push(bathroom4);
-array.push(bathroom5);
-array.push(bathroom6);
-array.push(bathroom7);
-array.push(bathroom8);
-array.push(bathroom9);
-array.push(bathroom10);
-array.push(bathroom11);
-array.push(bathroom12);
-array.push(bathroom13);
-
 // Initialize and add the map
 let map;
 
@@ -167,10 +138,8 @@ function geocodeBathroom(Bathroom) {
 
           }); 
         }
-        
-        for (let i = 0; i < array.length; i++) {
-          geocodeBathroom(array[i]);
-        }
+      
+        fetchBathrooms(); 
 
         function addPinToMap(lat, lng, Bathroom) {
             console.log("addPinToMap function called");
@@ -233,6 +202,12 @@ function geocodeBathroom(Bathroom) {
              } else{
                 document.getElementById("babyChanging").innerHTML = `Baby Changing Station Not Available`;
              }
+
+             if(Bathroom.getNotes()){
+                document.getElementById("notes").innerHTML = Bathroom.getNotes();
+              } else{
+                document.getElementById("notes").display = "none";
+              }
               
               popOut.style.display = "block"; // Show the popOut element
 
